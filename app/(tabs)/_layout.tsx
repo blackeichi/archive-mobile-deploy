7a23da1170eef0986bbc395f7efb143eb57b32ea
HanJeongWoo <87626378+blackeichi@users.noreply.github.com>
2026-04-17 20:20:24 +0900

    feat: layout tabs에 아이콘 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,5 @@
+import Entypo from "@expo/vector-icons/Entypo";
+import Octicons from "@expo/vector-icons/Octicons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
@@ -7,8 +9,33 @@ export default function TabsLayout() {
         headerShown: false,
       }}
     >
-      <Tabs.Screen name="index" options={{ title: "홈" }} />
-      <Tabs.Screen name="categories" options={{ title: "카테고리" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "홈",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Octicons
+              name={focused ? "home" : "home-fill"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="postsByCategory/[id]"
+        options={{
+          title: "포스트",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Entypo
+              name={focused ? "text-document" : "text-document-inverted"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      {/* <Tabs.Screen name="" options={{ title: "설정" }} /> */}
     </Tabs>
   );
 }
